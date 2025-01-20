825cb8341ef4ade085b649e23f743766461ef3c1
Bas Terwijn <bterwijn@gmail.com>
2025-01-20 05:42:16 +0100

    update

--- exercise/party_schedule_test.py
+++ exercise/party_schedule_test.py
@@ -2,10 +2,17 @@ import party_schedule
 import party_schedule_solution
 import io
 import contextlib
-import difflib
-import colorama
-from colorama import Fore, Style
 
+colorama_is_installed = False
+try:
+    import difflib
+    import colorama
+    from colorama import Fore, Style
+    colorama_is_installed = True
+except ImportError:
+    print("Optionally install 'colorama' for error highlighting using:\n")
+    print("  pip install colorama\n")
+    
 exercise_functions = [
     party_schedule.exercise1,
     party_schedule.exercise2,
@@ -29,7 +36,8 @@ solution_functions = [
 ]
 
 def main():
-    colorama.init()
+    if colorama_is_installed:
+        colorama.init()
     schedule1 = party_schedule.schedule
     schedule2 = party_schedule_solution.schedule
     all_correct = True
@@ -56,23 +64,26 @@ def schedule_to_string(schedule):
     return strbuf.getvalue()
 
 def print_error_message(str1, str2):
-    matcher = difflib.SequenceMatcher(None, str1, str2)
-    result1, result2 = [], []
-    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
-        if tag == 'equal': # Unchanged text
-            result1.append(str1[i1:i2])
-            result2.append(str2[j1:j2])
-        elif tag == 'replace': # Highlight replaced parts
-            result1.append(f"{Style.BRIGHT}{Fore.RED}{str1[i1:i2]}{Style.RESET_ALL}")
-            result2.append(f"{Style.BRIGHT}{Fore.GREEN}{str2[j1:j2]}{Style.RESET_ALL}")
-        elif tag == 'delete': # Highlight deletions in red
-            result1.append(f"{Style.BRIGHT}{Fore.RED}{str1[i1:i2]}{Style.RESET_ALL}")
-        elif tag == 'insert': # Highlight insertions in green
-            result2.append(f"{Style.BRIGHT}{Fore.GREEN}{str2[j1:j2]}{Style.RESET_ALL}")
-    print("=================== ERROR, Your schedule:")
-    print("".join(result1),end='')
-    print("=================== is not equal to correct schedule:")
-    print("".join(result2),end='')
+    if colorama_is_installed:
+        matcher = difflib.SequenceMatcher(None, str1, str2)
+        result1, result2 = [], []
+        for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+            if tag == 'equal': # Unchanged text
+                result1.append(str1[i1:i2])
+                result2.append(str2[j1:j2])
+            elif tag == 'replace': # Highlight replaced parts
+                result1.append(f"{Style.BRIGHT}{Fore.RED}{str1[i1:i2]}{Style.RESET_ALL}")
+                result2.append(f"{Style.BRIGHT}{Fore.GREEN}{str2[j1:j2]}{Style.RESET_ALL}")
+            elif tag == 'delete': # Highlight deletions in red
+                result1.append(f"{Style.BRIGHT}{Fore.RED}{str1[i1:i2]}{Style.RESET_ALL}")
+            elif tag == 'insert': # Highlight insertions in green
+                result2.append(f"{Style.BRIGHT}{Fore.GREEN}{str2[j1:j2]}{Style.RESET_ALL}")
+        str1 = "".join(result1)
+        str2 = "".join(result2)
+    print("=================== ERROR, your schedule:")
+    print(str1, end='')
+    print("=================== is not equal to the correct schedule:")
+    print(str2, end='')
 
 if __name__ == "__main__":
     main()
