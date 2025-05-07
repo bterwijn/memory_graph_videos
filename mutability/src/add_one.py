25bfd1d4eea392d9d354d4732318dbbb618a9eef
Bas Terwijn <bterwijn@gmail.com>
2025-05-07 21:27:22 +0200

    update

--- mutability/src/add_one.py
+++ mutability/src/add_one.py
@@ -1,11 +1,9 @@
 import memory_graph as mg
 
 def add_one(a, b, c):
-    mg.block(mg.show, mg.stack())
     a += [1]
     b += (1,)
     c += [1]
-    mg.block(mg.show, mg.stack())
 
 a = [4, 3, 2]
 b = (4, 3, 2)
