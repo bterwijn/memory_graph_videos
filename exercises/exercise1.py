f45a3e1144502f460bb836ef01e3f0f97ef84375
Bas Terwijn <bterwijn@gmail.com>
2025-07-17 00:26:03 +0200

    update

---
+++ exercises/exercise1.py
@@ -0,0 +1,11 @@
+import copy
+
+mydict = {'a': [100], 'b': [200], 'c': [300]}
+c1 = mydict
+c2 = mydict.copy()
+c3 = copy.deepcopy(mydict)
+c1['a'].append(111)
+c2['b'].append(222)
+c3['c'].append(333)
+
+print(mydict)
---
+++ exercises/exercise3.py
@@ -0,0 +1,10 @@
+
+tuple1  = (100, 200)
+tuple2  = tuple1
+tuple2 += (300,)
+
+list1   = [100, 200]
+list2   = list1
+list2  += [300]
+
+print(tuple1[-1], list1[-1])
