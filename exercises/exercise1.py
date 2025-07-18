c60c08d4b08bbaae1cd085109c68a537bc031e48
Bas Terwijn <bterwijn@gmail.com>
2025-07-18 20:00:53 +0200

    update

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,31 +1,21 @@
-import memory_graph as mg
+
+# What is the output of this program?
 import copy
 
-mydict = {'a': [100], 'b': [200], 'c': [300]}
+mydict = {1: [], 2: [], 3: []}
 c1 = mydict
 c2 = mydict.copy()
 c3 = copy.deepcopy(mydict)
-c1['a'].append(111)
-c2['b'].append(222)
-c3['c'].append(333)
+c1[1].append(100)
+c2[2].append(200)
+c3[3].append(300)
 
 print(mydict)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# --- possible answers ---
+# A) {1: [], 2: [], 3: []}
+# B) {1: [100], 2: [], 3: []}
+# C) {1: [100], 2: [200], 3: []}
+# D) {1: [100], 2: [200], 3: [300]}
 
 
 
