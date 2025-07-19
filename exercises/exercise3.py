909f2637f228745d35da773dea67099e96653885
Bas Terwijn <bterwijn@gmail.com>
2025-07-19 02:37:17 +0200

    update

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,4 +1,5 @@
 
+# Output of this Python program?
 tuple1  = (100, 200)
 tuple2  = tuple1
 tuple2 += (300,)
@@ -8,3 +9,8 @@ list2   = list1
 list2  += [300]
 
 print(tuple1[-1], list1[-1])
+# --- possible answers ---
+# A) 200 200
+# B) 200 300
+# C) 300 200
+# D) 300 300
