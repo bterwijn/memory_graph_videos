d9138626e9441300dbd0dcb3618ece966b55152b
Bas Terwijn <bterwijn@gmail.com>
2025-07-24 17:22:28 +0200

    update

---
+++ exercises/exercise7.py
@@ -0,0 +1,21 @@
+
+# What is the output of this Python program?
+def fun(a, b, c, d):
+    a += [1, 2]
+    b += (1, 2)
+    c |= {1, 2}
+    d |= frozenset([1, 2])
+
+a = [1]
+b = (1,)
+c = set([1])
+d = frozenset([1])
+fun(a, b, c, d)
+
+print(a, b, c, d)
+# --- possible answers ---
+# A) [1, 1, 2] (1,) {1, 2} frozenset({1})
+# B) [1, 1, 2] (1,) {1, 1, 2} frozenset({1})
+# C) [1, 1, 2] (1, 1, 2) {1, 2} frozenset({1, 2})
+# D) [1, 1, 2] (1, 1, 2) {1, 1, 2} frozenset({1, 1, 2})
+
