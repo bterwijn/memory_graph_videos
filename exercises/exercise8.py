167a2cb3e0f9a631ed755eb396531e9b97f065bd
Bas Terwijn <bterwijn@gmail.com>
2025-07-24 19:20:15 +0200

    update

--- exercises/exercise8.py
+++ exercises/exercise8.py
@@ -2,22 +2,23 @@
 # Output of this Python program?
 import copy
 
-def fun(cs):
-    for i, c in enumerate(cs):
-        c[0].append(i)
+def fun(c1, c2, c3, c4):
+    c1[0].append(1)
+    c2[0].append(2)
+    c3[0].append(3)
+    c4[0].append(4)
 
 mylist = [[]]
 c1 = mylist
 c2 = mylist.copy()
 c3 = copy.copy(mylist)
 c4 = copy.deepcopy(mylist)
-fun( (c1, c2, c3, c4) )
+fun(c1, c2, c3, c4)
 
 print(mylist)
 # --- possible answers ---
-# A) [[]]
-# B) [[0]]
-# C) [[0, 1]]
-# D) [[0, 1, 2]]
-# E) [[0, 1, 2, 3]]
+# A) [[1]]
+# B) [[1, 2]]
+# C) [[1, 2, 3]]
+# D) [[1, 2, 3, 4]]
 
