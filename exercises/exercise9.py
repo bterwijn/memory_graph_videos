2e5f0baa67a48ff79509121b19db032a7c0f6571
Bas Terwijn <bterwijn@gmail.com>
2025-07-27 15:18:33 +0200

    update

---
+++ exercises/exercise9.py
@@ -0,0 +1,19 @@
+
+# Output of this Python program?
+a = [1]
+b = a
+b += [2]
+b.append(3)
+b = b + [4]
+b.append(5)
+
+print(a)
+# --- possible answers ---
+# A) [1]
+# B) [1, 2]
+# C) [1, 2, 3]
+# D) [1, 2, 3, 4]
+# E) [1, 2, 3, 4, 5]
+
+
+
