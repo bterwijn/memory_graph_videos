e443704fca2bac6170f5acb01577498228422fb3
Bas Terwijn <bterwijn@gmail.com>
2025-07-29 23:11:23 +0200

    exercise5.py, back to question and answers

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -1,5 +1,5 @@
-import memory_graph as mg
 
+# Output of this Python program?
 a = ([1], [2])
 b = a
 b[0].append(11)
@@ -8,3 +8,8 @@ b[1].append(22)
 b[2].append(33)
 
 print(a)
+# --- possible answers ---
+# A) ([1], [2])
+# B) ([1, 11], [2])
+# C) ([1, 11], [2, 22])
+# D) ([1, 11], [2, 22], [3, 33])
