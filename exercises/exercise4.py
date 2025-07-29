c1399cf7ac6435ebd5f94a57a994814556b11c0f
Bas Terwijn <bterwijn@gmail.com>
2025-07-29 03:53:02 +0200

    update

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -1,8 +1,22 @@
 
-a = [100, 200, 300]
-b = []
-b.append(a)
-a.append(b)
-b.append(b)
+# Output of this Python program?
+float1 = 0.0  ; float2 = float1
+str1   = "0"  ; str2   = str1
+list1  = [0]  ; list2  = list1
+tuple1 = (0,) ; tuple2 = tuple1
+set1   = {0}  ; set2   = set1
 
-print(a[-1][-1][0][1])
+float2 += 1.1
+str2   += "1"
+list2  += [1]
+tuple2 += (1,)
+set2   |= {1}
+
+print(float1, str1, list1, tuple1, set1)
+# --- possible answers ---
+# A) 0.0 0 [0] (0,) {0}
+# B) 0.0 0 [0, 1] (0,) {0, 1}
+# C) 0.0 0 [0, 1] (0, 1) {0, 1}
+# D) 0.0 01 [0, 1] (0, 1) {0, 1}
+# E) 1.1 01 [0, 1] (0, 1) {0, 1}
+
