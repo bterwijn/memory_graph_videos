6be771868095b0944641be7e3c3945891db87c2c
Bas Terwijn <bterwijn@gmail.com>
2026-03-16 13:27:17 +0100

    exercise20.py added

---
+++ exercises/exercise20.py
@@ -0,0 +1,19 @@
+
+# Output of this Python program?
+a = [1]
+b = a
+b += [2]
+b += (3,)
+b.append(4)
+b.extend([5])
+b.extend((6,))
+b[len(b):] = [7]
+b = b + [8]
+
+print(a)
+# --- possible answers ---
+# A) [1]
+# B) [1, 2]
+# C) [1, 2, 3, 4, 5, 6, 7]
+# D) [1, 2, 3, 4, 5, 6, 7, 8]
+# E) None of the above
