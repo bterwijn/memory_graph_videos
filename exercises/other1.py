b11b61ef62c3f8b326f1f9e13e0752f014ea98f8
Bas Terwijn <bterwijn@gmail.com>
2026-08-13 15:27:51 +0200

    update

--- exercises/other1.py
+++ exercises/other1.py
@@ -3,16 +3,16 @@
 class Value:
     def __init__(self, value):
         self.value = value
-    def set(self, value):
+    def set_value(self, value):
         self.value = value
         
 v1 = Value(1)
 v2 = Value(2)
 myset = {v1}
 print(v1 in myset, end=' ')
-v2.set(1)
+v2.set_value(1)
 print(v2 in myset, end=' ')
-v1.set(2)
+v1.set_value(2)
 print(v1 in myset, end=' ')
 
 # --- possible answers ---
@@ -20,3 +20,4 @@ print(v1 in myset, end=' ')
 # B) True False False
 # C) True True False
 # D) True False True
+
